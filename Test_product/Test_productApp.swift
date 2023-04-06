0544369b5c54e0927d01e4489ff95183964c4d6d
gcamanes <guillaume.camanes.prestataire@wiztivi.com>
2023-04-06 19:52:06 +0200

    Initial Commit

---
+++ Test_product/Test_productApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_productApp.swift
+//  Test_product
+//
+//  Created by Guillaume Camanes on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Test_productApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
